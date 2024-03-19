94f764b62194c28bc4ecaec15392d032cd396315
Sleepingoff <justjh30@gmail.com>
2024-03-19 10:33:18 +0900

    cors - 라우터 설정 및 기본 페이지 추가

--- 2. Quick-Start/todo/src/main.ts
+++ 2. Quick-Start/todo/src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "@/assets/global.css"
+import App from "./App.vue";
+import router from "./router/router.config";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.mount("#app");

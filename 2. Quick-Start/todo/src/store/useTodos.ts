1fb57a012b10cbfabb82851c1d52a03a73dee94d
Sleepingoff <justjh30@gmail.com>
2024-03-22 18:13:24 +0900

    update - 할 일이 없을 때

--- 2. Quick-Start/todo/src/store/useTodos.ts
+++ 2. Quick-Start/todo/src/store/useTodos.ts
@@ -1,14 +1,13 @@
 const useTodos = () => {
   const getTodos = () => {
     const currentRawTodos = localStorage.getItem("todos");
-    if (!currentRawTodos) return [{ todo: "등록된 할 일이 없네요!", id: 0 }];
+    if (!currentRawTodos) return [];
     const currentTodos = JSON.parse(currentRawTodos);
     return currentTodos;
   };
 
   const setTodos = (newTodo: string) => {
     const date = new Date();
-
     const newTodos = { todo: newTodo, id: date.getTime() };
     const currentTodos = getTodos();
     currentTodos.push(newTodos);
